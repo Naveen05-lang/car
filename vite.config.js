a8c8dd531fec1cd3841a90d01cbf759ea88fdcab
Naveen05-lang <naveenthalari2000@gmail.com>
2025-04-28 17:50:46 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+
+// vite.config.js
+
+export default {
+  build: {
+    target: 'esnext', // or you can specify chrome92, firefox92, etc.
+    minify: 'esbuild',
+    // other build options
+  }
+}
